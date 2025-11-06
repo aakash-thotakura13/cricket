52a4ba0533dbb946a3c9b5841be2898866df4f02
Aakash Thotakura <aakashthotakura@gmail.com>
2025-11-06 19:44:45 +0530

    Modified: removed recoil import and RouterProvide tag from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,12 @@ import App from './App.jsx'
 
 import { RouterProvider } from 'react-router'
 import { allRoutes } from './routes/allRoutes.jsx'
-import { RecoilRoot } from 'recoil'
 
 
 createRoot(document.getElementById('root')).render(
-  <RecoilRoot>
+  <>
     <RouterProvider router={allRoutes}>
       <App />
     </RouterProvider>
-  </RecoilRoot>
+  </>
 )
