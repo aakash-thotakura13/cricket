2f7ed00b291a0bc82e95d98fc96420e23a4c6619
aakash-thotakura13 <aakashthotakura@gmail.com>
2025-11-07 23:13:32 +0530

    Completed: ScoreBar.jsx component

--- src/components/ScoreBar.jsx
+++ src/components/ScoreBar.jsx
@@ -1,16 +1,21 @@
 import addRuns from "../function/addRuns";
+import wicketsCounter from "../function/wicketsCounter";
 
-export const ScoreBar = ({ team, allOvers }) => {
+export const ScoreBar = ({ team, allOvers, activeOver }) => {
 
   const overs = allOvers?.map(over => over.overRuns)?.flat();
-  const overCount = allOvers.length;
-  const deliveryCount = overs.length;
+  
+  const totalRuns = addRuns([...overs, ...activeOver])
+  const wickets = wicketsCounter([...overs, ...activeOver]);
+
+  const overCount = allOvers?.length;
+  const activeOverLength = activeOver?.length;
 
   return (
-    <p style={{ fontSize: "1.2em", display: "flex", justifyContent: "space-between", }}>
+    <p style={{ fontSize: "1.2em", display: "flex", justifyContent: "space-between", alignItems: "center", backgroundColor: "#ccc", color: "black", margin: "1em 0em", padding: "0em 0.5em", borderRadius: "0.35em 0.35em 0.7em 0.7em", }}>
       <span>{team.tag}</span>
-
-      <span>{addRuns(overs)}/{allOvers.length}.{overs.length - (allOvers.length * 6)}</span>
+      <span style={{ fontSize: "0.9rem" }}>RunRate: {(totalRuns / `${overCount}.${activeOverLength}`).toFixed(2)}</span>
+      <span>{totalRuns} / {wickets} </span>
     </p>
   )
 }
