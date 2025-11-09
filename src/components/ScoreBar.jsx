823277e97aaacbc73b6f5324c7df7c1ad876b7c7
aakash-thotakura13 <aakashthotakura@gmail.com>
2025-11-09 15:45:50 +0530

    Modified: ScoreBar.jsx file, added - RunRate

--- src/components/ScoreBar.jsx
+++ src/components/ScoreBar.jsx
@@ -12,10 +12,10 @@ export const ScoreBar = ({ team, allOvers, activeOver }) => {
   const activeOverLength = activeOver?.length;
 
   return (
-    <p style={{ fontSize: "1.2em", display: "flex", justifyContent: "space-between", alignItems: "center", backgroundColor: "#ccc", color: "black", margin: "1em 0em", padding: "0em 0.5em", borderRadius: "0.35em 0.35em 0.7em 0.7em", }}>
-      <span>{team.tag}</span>
+    <p style={{ fontSize: "1em", display: "flex", justifyContent: "space-between", alignItems: "center", backgroundColor: "#ccc", color: "black", margin: "1em 0em", padding: "0em 0.5em", borderRadius: "0.35em 0.35em 0.7em 0.7em", }}>
+      <span style={{ fontWeight:"500", fontSize:"1.1em" }}>{team.tag}: {totalRuns}/{wickets}</span>
       <span style={{ fontSize: "0.9rem" }}>RunRate: {(totalRuns / `${overCount}.${activeOverLength}`).toFixed(2)}</span>
-      <span>{totalRuns} / {wickets} </span>
+      <span style={{ fontSize: "0.9rem" }}>Over: {overCount}.{activeOverLength}</span>
     </p>
   )
 }
