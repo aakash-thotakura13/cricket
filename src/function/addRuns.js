6821455f74edd2fd269a3cc7ec5442fe01588652
aakash-thotakura13 <aakashthotakura@gmail.com>
2025-11-11 11:44:19 +0530

    modified addRuns() for cleaner-simple-readable code

--- src/function/addRuns.js
+++ src/function/addRuns.js
@@ -1,12 +1,5 @@
-
-export default function addRuns(runs) {
-
-  if (runs === undefined || runs.length === 0) {
-    return 0;
-  } else {
-    const updateRuns = runs.map(run => typeof run === "string" ? 0 : run);
-    return updateRuns.reduce((acc, run) => acc + run, 0);
-  }
-
-
+export default function addRuns(runs = []) {
+  return runs
+    .filter((run) => typeof run === "number")
+    .reduce((total, run) => total + run, 0);
 }
