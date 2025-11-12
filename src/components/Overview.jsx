1ccd3fe79d399cb891d623f3789d1c1a141f7ae5
aakash-thotakura13 <aakashthotakura@gmail.com>
2025-11-13 00:36:57 +0530

    spread the battingCard and bowlingCard array so that the sort would not mutate the array directly, but would create a copy of the array that can be modified before being displayed

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,8 +1,8 @@
 
 export const Overview = ({ battingCard, bowlingCard }) => {
 
-  const filterBatsman = battingCard.length > 0 && battingCard.sort((a, b) => b.totalRuns - a.totalRuns).slice(0, 3) || []
-  const filterBowler = bowlingCard.length > 0 && bowlingCard.sort((a, b) => b.runsConceded - a.runsConceded).sort((a, b) => b.wickets - a.wickets).slice(0, 3) || []
+  const filterBatsman = battingCard.length > 0 && [...battingCard].sort((a, b) => b.totalRuns - a.totalRuns).slice(0, 3) || []
+  const filterBowler = bowlingCard.length > 0 && [...bowlingCard].sort((a, b) => b.runsConceded - a.runsConceded).sort((a, b) => b.wickets - a.wickets).slice(0, 3) || []
 
   return (
 
@@ -42,10 +42,12 @@ const styles = {
     display: "flex",
     justifyContent: "space-between",
     margin: "0.5em 1.4em",
+    fontSize:"0.9em",
   },
   sectionTitle: {
     width: "90%",
     margin: "0.75em auto",
+    fontSize:"0.95em",
     backgroundColor: "#ccc",
     color: "black",
     textShadow: "1px 1px 1px black",
