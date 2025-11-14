ec467d3bf899196ceb4b7ee057d4e15db54c49a7
aakash-thotakura13 <aakashthotakura@gmail.com>
2025-11-14 18:35:04 +0530

    created: PageTitle component to display and maintain the title via a single component

---
+++ src/components/PageTitle.jsx
@@ -0,0 +1,8 @@
+
+export const PageTitle = ({ title }) => {
+  return (
+    <header style={{ backgroundColor: "#ccc", color: "#777", textShadow:"1px 1px 2px black", borderRadius: "5em", textAlign: "center", }}>
+      <h1>{title}</h1>
+    </header>
+  )
+}
