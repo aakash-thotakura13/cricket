24a33c4615b054dcb8a8d54bf47803e6f712c26c
aakash-thotakura13 <aakashthotakura@gmail.com>
2025-11-14 18:36:34 +0530

    created: Graphs page to display overs/runs, run-rate graphs

---
+++ src/pages/Graphs.jsx
@@ -0,0 +1,10 @@
+import { PageTitle } from "../components/PageTitle"
+
+export const Graphs = () => {
+  return (
+    <div>
+      <PageTitle title="Graphs" />
+      {/* <PageNavBar displayInnings={displayInnings} setDisplayInnings={setDisplayInnings} /> */}
+    </div>
+  )
+}
