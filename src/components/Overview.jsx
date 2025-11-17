c6ae8de72aced35537f75aedd8b98f07f3919e6b
Aakash Thotakura <aakashthotakura@gmail.com>
2025-11-17 21:09:29 +0530

    modified Overview component to display both innings' overview next to the Pitch component

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,16 +1,58 @@
+import { useAtomValue } from "jotai";
 
-export const Overview = ({ battingCard, bowlingCard }) => {
+import {
+  inningsOneAllOvers, inningsOneBattingScoreCard, inningsOneBowlingScoreCard,
+  inningsTwoAllOvers, inningsTwoBattingScoreCard, inningsTwoBowlingScoreCard,
+  overRuns, teamOne, teamTwo
+} from "../jotai/atom";
 
-  const filterBatsman = battingCard.length > 0 && [...battingCard].sort((a, b) => b.totalRuns - a.totalRuns).slice(0, 3) || []
-  const filterBowler = bowlingCard.length > 0 && [...bowlingCard].sort((a, b) => b.runsConceded - a.runsConceded).sort((a, b) => b.wickets - a.wickets).slice(0, 3) || []
+import addRuns from "../function/addRuns";
+import wicketsCounter from "../function/wicketsCounter";
+
+
+export const Overview = () => {
+
+  const _teamOne = useAtomValue(teamOne);
+  const _teamTwo = useAtomValue(teamTwo);
+
+  const _inningsOneBattingScoreCard = useAtomValue(inningsOneBattingScoreCard);
+  const _inningsOneBowlingScoreCard = useAtomValue(inningsOneBowlingScoreCard);
+
+  const _inningsTwoBattingScoreCard = useAtomValue(inningsTwoBattingScoreCard);
+  const _inningsTwoBowlingScoreCard = useAtomValue(inningsTwoBowlingScoreCard);
+
+  const _inningsOneAllOvers = useAtomValue(inningsOneAllOvers);
+  const _inningsTwoAllOvers = useAtomValue(inningsTwoAllOvers);
+
+  const _overRuns = useAtomValue(overRuns);
+
 
   return (
 
     <div style={styles.container}>
 
-      <h3>Overview</h3>
+      <InningsOverView battingCard={_inningsOneBattingScoreCard} bowlingCard={_inningsOneBowlingScoreCard} battingTeamName={_teamOne.tag} bowlingTeamName={_teamTwo.tag} oversArray={[..._inningsOneAllOvers, _overRuns].flat()} />
+      <InningsOverView battingCard={_inningsTwoBattingScoreCard} bowlingCard={_inningsTwoBowlingScoreCard} battingTeamName={_teamTwo.tag} bowlingTeamName={_teamOne.tag} oversArray={[..._inningsTwoAllOvers, _overRuns].flat()} />
 
-      <p style={styles.sectionTitle}>Batting</p>
+    </div>
+  )
+};
+
+function InningsOverView({ battingCard, bowlingCard, battingTeamName, bowlingTeamName, oversArray }) {
+
+  const filterBatsman = battingCard?.length > 0 && [...battingCard]?.sort((a, b) => b.totalRuns - a.totalRuns)?.slice(0, 3) || [];
+  const filterBowler = bowlingCard?.length > 0 && [...bowlingCard]?.sort((a, b) => b.totalRuns - a.totalRuns)?.sort((a, b) => b.wickets - a.wickets)?.slice(0, 3) || [];
+
+  const runsFlatArray = oversArray.map(over => over.overRuns).flat();
+  const score = addRuns(runsFlatArray);
+  const wickets = wicketsCounter(runsFlatArray);
+
+  return (
+    <div>
+      <p style={styles.sectionTitle}>
+        <span>{battingTeamName}</span>
+        <span>{score} / {wickets}</span>
+      </p>
 
       {
         filterBatsman.map((batsman, id) => <p key={id} style={styles.subContainer}>
@@ -18,17 +60,19 @@ export const Overview = ({ battingCard, bowlingCard }) => {
         </p>)
       }
 
-      <p style={styles.sectionTitle}>Bowling</p>
+      <p style={styles.sectionTitle}>
+        <span>{bowlingTeamName}</span>
+      </p>
 
       {
         filterBowler.map((bowler, id) => <p key={id} style={styles.subContainer}>
-          <span>{bowler.playerName}</span> <span>{bowler.wickets} / {bowler.runsConceded}</span>
+          <span>{bowler.playerName}</span> <span>{bowler.wickets} / {bowler.totalRuns}</span>
         </p>)
       }
 
     </div>
   )
-};
+}
 
 
 const styles = {
@@ -42,16 +86,19 @@ const styles = {
     display: "flex",
     justifyContent: "space-between",
     margin: "0.5em 1.4em",
-    fontSize:"0.9em",
+    fontSize: "0.9em",
   },
   sectionTitle: {
     width: "90%",
     margin: "0.75em auto",
-    fontSize:"0.95em",
+    fontSize: "0.95em",
+    fontWeight: "600",
     backgroundColor: "#ccc",
     color: "black",
-    textShadow: "1px 1px 1px black",
     borderRadius: "1em",
     textAlign: "center",
+    display: "flex",
+    justifyContent: "space-between",
+    padding: "0.25em 0.5em",
   },
 };
