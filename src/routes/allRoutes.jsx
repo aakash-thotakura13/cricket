6899fcf4d72f7b321be01b2f1736f2bc41487b93
Aakash Thotakura <aakashthotakura@gmail.com>
2025-11-18 15:42:13 +0530

    added lazy loading option

--- src/routes/allRoutes.jsx
+++ src/routes/allRoutes.jsx
@@ -1,42 +1,26 @@
 import { createBrowserRouter } from "react-router";
+import { lazy } from "react";
 import App from "../App";
-import { SelectTeam   } from "../pages/SelectTeam";
-import { ScoreCard    } from "../pages/ScoreCard";
-import { OverDetails  } from "../pages/OverDetails";
-import { Partnerships } from "../pages/Partnerships";
-import { Graphs       } from "../pages/Graphs";
-import { Comparison   } from "../pages/Comparison";
 
+// Lazy load named exports
+const SelectTeam   = lazy(() => import("../pages/SelectTeam"  ).then((m) => ({ default: m.SelectTeam })));
+const ScoreCard    = lazy(() => import("../pages/ScoreCard"   ).then((m) => ({ default: m.ScoreCard })));
+const OverDetails  = lazy(() => import("../pages/OverDetails" ).then((m) => ({ default: m.OverDetails })));
+const Partnerships = lazy(() => import("../pages/Partnerships").then((m) => ({ default: m.Partnerships })));
+const Graphs       = lazy(() => import("../pages/Graphs"      ).then((m) => ({ default: m.Graphs })));
+const Comparison   = lazy(() => import("../pages/Comparison"  ).then((m) => ({ default: m.Comparison })));
 
 export const allRoutes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      {
-        path: "select",
-        element: <SelectTeam />,
-      },
-      {
-        path: "scorecard",
-        element: <ScoreCard />,
-      },
-      {
-        path: "partnership",
-        element: <Partnerships />
-      },
-      {
-        path: "overs",
-        element: <OverDetails />
-      },
-      {
-        path: "graphs",
-        element: <Graphs />,
-      },
-      {
-        path: "comparison",
-        element: <Comparison />
-      },
+      { path: "select",      element: <SelectTeam />   },
+      { path: "scorecard",   element: <ScoreCard />    },
+      { path: "partnership", element: <Partnerships /> },
+      { path: "overs",       element: <OverDetails />  },
+      { path: "graphs",      element: <Graphs />       },
+      { path: "comparison",  element: <Comparison />   },
     ],
-  }
+  },
 ]);
