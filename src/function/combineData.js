0d307b7691f5df7978e0cdb55c688bdad64aa775
Aakash Thotakura <aakashthotakura@gmail.com>
2025-11-18 11:34:45 +0530

    Modified: add a new calculation to get the total wickets till the given over

--- src/function/combineData.js
+++ src/function/combineData.js
@@ -3,36 +3,41 @@ import wicketsCounter from "./wicketsCounter";
 
 export function combineData(inningsOne, inningsTwo) {
 
-    const one = [...inningsOne].map(over => over.overRuns).map(over => addRuns(over)) || [];
-    const two = [...inningsTwo].map(over => over.overRuns).map(over => addRuns(over)) || [];
+  const one = [...inningsOne].map(over => over.overRuns).map(over => addRuns(over)) || [];
+  const two = [...inningsTwo].map(over => over.overRuns).map(over => addRuns(over)) || [];
 
-    let tempArr = [];
+  const oneWicket = [...inningsOne].map(over => over.overRuns).flat() || 0;
+  const twoWicket = [...inningsTwo].map(over => over.overRuns).flat() || 0;
 
-    for (let i = 0; i < Math.max(inningsOne.length, inningsTwo.length); i++) {
+  let tempArr = [];
 
-      const oneTotalRuns = addRuns(one.slice(0, i + 1));
-      const oneRunRate = oneTotalRuns / (i + 1);
-      const oneWickets = inningsOne[i] ? wicketsCounter(inningsOne[i].overRuns) : 0;
+  for (let i = 0; i < Math.max(inningsOne.length, inningsTwo.length); i++) {
 
-      const twoTotalRuns = addRuns(two.slice(0, i + 1));
-      const twoRunRate = twoTotalRuns / (i + 1);
-      const twoWickets = inningsTwo[i] ? wicketsCounter(inningsTwo[i].overRuns) : 0;
+    const oneTotalRuns = addRuns(one.slice(0, i + 1));
+    const oneTotalWickets = wicketsCounter(oneWicket.slice(0, 6 * (i + 1)));
+    const oneRunRate = oneTotalRuns / (i + 1);
+    const oneWickets = inningsOne[i] ? wicketsCounter(inningsOne[i].overRuns) : 0;
 
-      let entry = {
-        over: i + 1,
-        oneRun: one[i] || 0,
-        twoRun: two[i] || 0,
-        oneTotalRuns,
-        twoTotalRuns,
-        oneRunRate,
-        twoRunRate,
-        oneWickets,
-        twoWickets
-      }
+    const twoTotalRuns = addRuns(two.slice(0, i + 1));
+    const twoTotalWickets = wicketsCounter(twoWicket.slice(0, 6 * (i + 1)));
+    const twoRunRate = twoTotalRuns / (i + 1);
+    const twoWickets = inningsTwo[i] ? wicketsCounter(inningsTwo[i].overRuns) : 0;
 
-      tempArr.push(entry);
+    console.log(oneWicket.slice(0, 6 * (i + 1)));
 
+    let entry = {
+      over: i + 1,
+      oneRun: one[i] || 0,
+      twoRun: two[i] || 0,
+      oneTotalRuns, twoTotalRuns,
+      oneTotalWickets, twoTotalWickets,
+      oneRunRate, twoRunRate,
+      oneWickets, twoWickets
     }
 
-    return tempArr
-  };
+    tempArr.push(entry);
+
+  }
+
+  return tempArr
+};
