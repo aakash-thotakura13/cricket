25316a6b6b4f5f97ef31ea4a000425014a350eb2
Aakash Thotakura <aakashthotakura@gmail.com>
2025-11-18 17:53:51 +0530

    modified: ScoreBar.jsx file fontSizes

--- src/components/ScoreBar.jsx
+++ src/components/ScoreBar.jsx
@@ -12,10 +12,10 @@ export const ScoreBar = ({ team, allOvers, activeOver }) => {
   const activeOverLength = activeOver?.length;
 
   return (
-    <p style={{ fontSize: "1em", display: "flex", justifyContent: "space-between", alignItems: "center", backgroundColor: "#ccc", color: "black", margin: "1em 0em", padding: "0em 0.5em", borderRadius: "0.35em 0.35em 0.7em 0.7em", }}>
-      <span style={{ fontWeight:"500", fontSize:"1.1em" }}>{team.tag}: {totalRuns}/{wickets}</span>
-      <span style={{ fontSize: "0.9rem" }}>RunRate: {(totalRuns / `${overCount}.${activeOverLength}`).toFixed(2)}</span>
-      <span style={{ fontSize: "0.9rem" }}>Over: {overCount}.{activeOverLength}</span>
+    <p style={{ fontSize: "1em", display: "flex", justifyContent: "space-between", alignItems: "end", backgroundColor: "#ccc", color: "black", margin: "1em 0em", padding: "0.7em 1.4em", borderRadius: "3em", }}>
+      <span style={{ fontWeight:"500", fontSize:"1.3em" }}>{team.tag}: {totalRuns}/{wickets}</span>
+      <span style={{ fontSize: "1em" }}>RR: {(totalRuns / `${overCount}.${activeOverLength}`).toFixed(2)}</span>
+      <span style={{ fontSize: "1.1em" }}>Ovr: {overCount}.{activeOverLength}</span>
     </p>
   )
 }
