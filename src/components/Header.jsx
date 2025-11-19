5a57172bbaca2c5c5ffd7c8cb60c46cc46a51ace
aakash-thotakura13 <aakashthotakura@gmail.com>
2025-11-19 21:36:28 +0530

    modified: set up showGame to toggle between 'Select' and 'Game', added border-bottom, top and radius for active route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,55 +3,62 @@ import { NavLink } from "react-router"
 import { teamOne, teamTwo } from "../jotai/atom"
 
 const definedRoutes = [
-  {
-    path: "/select",
-    element: "Select",
-  },
-  {
-    path: "/scorecard",
-    element: "Scorecards",
-  },
-  {
-    path: "/partnership",
-    element: "Partnerships",
-  },
-  {
-    path: "/overs",
-    element: "Overs",
-  },
-  {
-    path: "/graphs",
-    element: "Graphs",
-  },
-  {
-    path: "/comparison",
-    element: "Comparisons",
-  },
-]
+  { path: "/select", element: "Select", },
+  { path: "/scorecard", element: "Scorecards", },
+  { path: "/partnership", element: "Partnerships", },
+  { path: "/overs", element: "Overs", },
+  { path: "/graphs", element: "Graphs", },
+  { path: "/comparison", element: "Comparisons", },
+];
 
 export default function HeaderComponent() {
 
-  const _teamOne = useAtom(teamOne)
-  const _teamTwo = useAtom(teamTwo)
+  const [teamOneValue] = useAtom(teamOne);
+  const [teamTwoValue] = useAtom(teamTwo);
+
+  const showGame = Object.keys(teamOneValue).length > 0 && Object.keys(teamTwoValue).length > 0;
 
   return (
-    <div>
-      <nav style={{ display: "flex", justifyContent: "space-between", overflowY: "hidden", scrollbarWidth: "0.1em", scrollbars: "none" }}>
-        {
-          definedRoutes.map((route, id) =>
-            <NavLink
-              key={id}
-              to={route.path}
-              style={({ isActive }) => ({ textDecoration: "none",  padding: "0.35em 0.7em", color: "white", fontWeight: isActive ? "bold" : "normal", borderBottom: isActive ? "3px solid white" : "none", })}
-            >
-              {route.element === "Select"
-                ? (Object.keys(_teamOne[0]).length > 0 && Object.keys(_teamTwo[0]).length > 0) ? "Game" : route.element
-                : route.element}
-            </NavLink>
-          )
-        }
-      </nav>
-
-    </div>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        overflowY: "hidden",
+        position: "sticky",
+        top: 0, borderBottom: "2.5px solid #ccc",
+        backgroundColor: "#1c1c1c",
+        zIndex: 1000,
+        padding: "0.35em 0.7em",
+      }}
+    >
+      {
+        definedRoutes.map((route, id) =>
+          <NavLink
+            key={id}
+            to={route.path}
+            style={({ isActive }) => ({
+              textDecoration: "none",
+              padding: "0.25em 0.5em",
+              color: "#fff",
+              fontWeight: isActive ? "bold" : "normal",
+              borderTop: isActive ? "1.75px solid grey" : "1px solid transparent",
+              borderBottom: isActive ? "1.75px solid grey" : "1px solid transparent",
+              backgroundColor: isActive ? "#3c3c3c" : "transparent",
+              borderRadius: "1em",
+            })}
+          >
+            {
+              route.element === "Select"
+                ? (
+                  showGame
+                    ? "Game"
+                    : route.element
+                )
+                : route.element
+            }
+          </NavLink>
+        )
+      }
+    </nav>
   )
-}
+};
