c8932b5254de1bbdb98a7c9b8755eaf0033e8e65
Aakash Thotakura <aakashthotakura@gmail.com>
2025-11-19 13:11:30 +0530

    added ResponsiveContainer, since the 'responsive' tag for the <Bar> tag is not of much use

--- src/components/chart/SimpleBarChart.jsx
+++ src/components/chart/SimpleBarChart.jsx
@@ -1,37 +1,27 @@
-import { Bar, BarChart, Legend, Rectangle, Tooltip, XAxis, YAxis } from "recharts";
+import { Bar, BarChart, Legend, Rectangle, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
 export const SimpleBarChart = ({ data, dataKeyOne, dataKeyTwo, XAxisKey, colorOne, colorTwo }) => {
 
   const xTicks = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
 
   return (
-    <BarChart
-      style={{ width: '100%', maxWidth: '350px', maxHeight: '70vh', aspectRatio: 1.618 }}
-      responsive
-      data={data}
-      margin={{
-        top: 5,
-        right: 0,
-        left: 0,
-        bottom: 5,
-      }}
-    >
-      {/* <CartesianGrid strokeDasharray="3 3" /> */}
-      <XAxis dataKey={XAxisKey} ticks={xTicks} />
-      <YAxis width="auto" />
-      <Tooltip />
-      <Legend iconSize={7} />
-      <Bar
-        dataKey={dataKeyOne}
-        fill={colorOne}
-        activeBar={<Rectangle />}
-        />
-      <Bar
-        dataKey={dataKeyTwo}
-        fill={colorTwo}
-        activeBar={<Rectangle />}
-      />
+    <div style={{ width: "100%", maxWidth: "350px", margin: "auto", }}>
+      <ResponsiveContainer width="100%" aspect={1.4}>
+        <BarChart
+          data={data}
+          margin={{ top: 5, right: 0, left: 0, bottom: 5, }}
+        >
+          {/* <CartesianGrid strokeDasharray="3 3" /> */}
+          <XAxis dataKey={XAxisKey} ticks={xTicks} />
+          <YAxis width="auto" />
+          <Tooltip />
+          <Legend iconSize={7} />
 
-    </BarChart>
-  )
+          <Bar dataKey={dataKeyOne} fill={colorOne} activeBar={<Rectangle />} />
+          <Bar dataKey={dataKeyTwo} fill={colorTwo} activeBar={<Rectangle />} />
+
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
