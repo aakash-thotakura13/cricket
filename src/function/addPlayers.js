8b8837ee48bf86549065efe305c08e8c7a76f8bf
aakash-thotakura13 <aakashthotakura@gmail.com>
2025-11-21 12:19:29 +0530

    modified: addPlayer - addBowler() so that the player who bowled the previous over would not be selected again

--- src/function/addPlayers.js
+++ src/function/addPlayers.js
@@ -8,62 +8,67 @@ export function addBatsman(
   setPartnerTwo,
   battingCard,
   setBattingCard,
-  outPlayer,
+  outPlayer
 ) {
-
-  const isEmpty = p => !p || Object.keys(p).length === 0;
+  const isEmpty = (p) => !p || Object.keys(p).length === 0;
 
   const alreadyExists = battingCard.some((p) => p.playerName === player);
   if (alreadyExists) {
     alert(`${player} is already in the batting card`);
     return;
+  }
+
+  const playerEntry = {
+    playerName: player,
+    runs: [],
+    status: "not out",
+    totalRuns: 0,
+    totalDeliveries: 0,
+    strikeRate: 0.0,
+    fours: 0,
+    sixes: 0,
   };
 
-  const playerEntry = { playerName: player, runs: [], status: "not out", totalRuns: 0, totalDeliveries: 0, strikeRate: 0.0, fours: 0, sixes: 0, };
-
   if (outPlayer === "playerOne") {
-    
     setPlayerOne(playerEntry);
     setPartnerOne(playerEntry);
-
   } else if (outPlayer === "playerTwo") {
-    
     setPlayerTwo(playerEntry);
     setPartnerTwo(playerEntry);
-
   } else {
-
     if (isEmpty(_playerOne)) {
-      
       setPlayerOne(playerEntry);
       setPartnerOne(playerEntry);
-
     } else if (isEmpty(_playerTwo)) {
-      
       setPlayerTwo(playerEntry);
       setPartnerTwo(playerEntry);
-
     } else {
-
-      alert("Both batsman are already selected")
+      alert("Both batsman are already selected");
       return;
-
     }
   }
 
   setBattingCard([...battingCard, playerEntry]);
-};
+}
 
-export function addBowler(player, setBowler, bowlingCard, setBowlingCard) {
+export function addBowler(
+  player,
+  prevBowler,
+  setBowler,
+  bowlingCard,
+  setBowlingCard
+) {
+  // prevent same bowler from bowling consecutive overs
+  if (prevBowler?.playerName === player) {
+    alert(`${player} cannot bowl consecutive overs.`);
+    return;
+  }
 
   const findBowler = bowlingCard.find((bowler) => bowler.playerName === player);
 
   if (findBowler) {
-
     setBowler(findBowler);
-
   } else {
-
     const newBowler = {
       playerName: player,
       runs: [],
@@ -72,6 +77,5 @@ export function addBowler(player, setBowler, bowlingCard, setBowlingCard) {
 
     setBowler(newBowler);
     setBowlingCard([...bowlingCard, newBowler]);
-
   }
 }
